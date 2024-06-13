0ce20a158874b63bc05c03b9b0082d34856af68d
santi0804 <santama.1988@gmail.com>
2024-06-13 16:50:12 -0500

    Inicio de API

--- Mi primera chanva/src/App.jsx
+++ Mi primera chanva/src/App.jsx
@@ -3,17 +3,23 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-  const [cedula, setCedula] = useState('');
-  const [placa, setPlaca] = useState('');
-  const [marca, setMarca] = useState('');
+  const [id_producto, setProducto] = useState('');
+  const [nombre_p, setNombre] = useState('');
+  const [referencia_p, setReferencia] = useState('');
+  const [valor_p, setValor] = useState('');
+  const [mes_de_consumo, setMesConsumo] = useState('');
+  const [fecha, setFecha] = useState('');
   const [updated, setUpdated] = useState('');
 
-  const handleChange1 = (event) => setCedula(event.target.value);
-  const handleChange2 = (event) => setPlaca(event.target.value);
-  const handleChange3 = (event) => setMarca(event.target.value);
+  const handleChange1 = (event) => setProducto(event.target.value);
+  const handleChange2 = (event) => setNombre(event.target.value);
+  const handleChange3 = (event) => setReferencia(event.target.value);
+  const handleChange4 = (event) => setValor(event.target.value);
+  const handleChange5 = (event) => setMesConsumo(event.target.value);
+  const handleChange6 = (event) => setFecha(event.target.value);
 
   const handleClick = () => {
-    setUpdated(`Registro 1: ${cedula}, Registro 2: ${placa}, Registro 3: ${marca}`);
+    setUpdated(`- ${id_producto}, - ${nombre_p}, - ${referencia_p}, - ${valor_p}, - ${mes_de_consumo}, - ${fecha}`, );
   };
 
   return (
@@ -25,25 +31,45 @@ function App() {
       </div>
 
       <h1>REGISTRO DE VISITANTE</h1>
-
+      <br />
+      <br />
       <div className="container">
+        <div className='container2'>
         <div className="registro">
-          <label htmlFor="cedula" className='inputCedula'>CEDULA: </label>
-          <input type="text" id="cedula" value={cedula} onChange={handleChange1} />
+          <label htmlFor="cedula" className="labelCedula">CEDULA: </label>
+          <input  className="inputCedula"  type="text" id="cedula" value={id_producto} onChange={handleChange1} />
         </div>
 
         <div className="registro">
-          <label htmlFor="placa" className='inputPlacas'>PLACAS: </label>
-          <input type="text" id="placa" value={placa} onChange={handleChange2} />
+          <label htmlFor="placa" className='LabelNombre'>NOMBRE: </label>
+          <input  className='inputNombre'type="text" id="nombre" value={nombre_p} onChange={handleChange2} />
         </div>
 
         <div className="registro">
-          <label htmlFor="marca" className='inputMarca'>MARCA:</label>
-          <input type="text" id="marca" value={marca} onChange={handleChange3} />
+          <label htmlFor="marca" className='labelMarca'>MARCA:</label>
+          <input className='inputMarca' type="text" id="marca" value={referencia_p} onChange={handleChange3} />
         </div>
 
+        <div className="registro">
+          <label htmlFor="marca" className='labelValor'>VALOR:</label>
+          <input className='inputValor' type="text" id="valor" value={valor_p} onChange={handleChange4} />
+        </div>
+
+        <div className="registro">
+          <label htmlFor="marca" className='labelMes'>MES:</label>
+          <input className='inputMes' type="text" id="mes" value={mes_de_consumo} onChange={handleChange5} />
+        </div>
+
+        <div className="registro">
+          <label htmlFor="marca" className='labelFecha'>FECHA:</label>
+          <input className='inputFecha' type="date" id="fecha" value={fecha} onChange={handleChange6} />
+        </div>
+
+        </div>
+
+        <br />
         <div className="card">
-          <button onClick={handleClick}>SOLICITAR</button>
+          <button onClick={handleClick}>REGISTRAR</button>
         </div>
       </div>
 
